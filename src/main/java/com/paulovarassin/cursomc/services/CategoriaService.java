306e35dedfdc0935cc4ad67c0f5518c59c0b802c
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-08-26 21:24:24 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/paulovarassin/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.paulovarassin.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulovarassin.cursomc.domain.Categoria;
+import com.paulovarassin.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
