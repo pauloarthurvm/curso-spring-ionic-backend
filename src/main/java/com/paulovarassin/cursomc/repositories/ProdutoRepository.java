a8c3ce8a2de85eef4db49e2b76f4f11b60ea088c
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-08-26 22:02:52 -0300

    Produto e Associacao Muitos-para-Muitos

---
+++ src/main/java/com/paulovarassin/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.paulovarassin.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paulovarassin.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	 
+}
