d9cf6309743a825b62175fff652fd8afbd4c0a5f
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-09-15 19:31:42 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/paulovarassin/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/paulovarassin/cursomc/dto/ClienteNewDTO.java
@@ -2,21 +2,45 @@ package com.paulovarassin.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.paulovarassin.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
+	@NotEmpty
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
 	private String nome;
+	
+	@NotEmpty
+	@Email(message = "Email inválido.")
 	private String email;
+
+	@NotEmpty
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 
+	@NotEmpty
 	private String logradouro;
+
+	@NotEmpty
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+
+	@NotEmpty
 	private String cep;
-	
+
+	@NotEmpty
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
