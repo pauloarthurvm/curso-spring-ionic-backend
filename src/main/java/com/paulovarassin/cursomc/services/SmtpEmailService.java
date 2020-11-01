f75746cfb97e15d8aa63735c4fb822ac8d312fe4
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-01 16:16:10 -0300

    Email HTML

--- src/main/java/com/paulovarassin/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/paulovarassin/cursomc/services/SmtpEmailService.java
@@ -1,15 +1,21 @@
 package com.paulovarassin.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractMailService{
 	
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 	
@@ -19,5 +25,14 @@ public class SmtpEmailService extends AbstractMailService{
 		mailSender.send(msg);
 		LOG.info("Email enviado");
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+	
+	
 
 }
