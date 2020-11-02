37f2e2ca58811ebb3eb505a32eb0238d9bf65fb9
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-02 15:10:06 -0300

    Esqueci a senha

--- src/main/java/com/paulovarassin/cursomc/services/AbstractMailService.java
+++ src/main/java/com/paulovarassin/cursomc/services/AbstractMailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.paulovarassin.cursomc.domain.Cliente;
 import com.paulovarassin.cursomc.domain.Pedido;
 
 public abstract class AbstractMailService implements EmailService{
@@ -70,4 +71,19 @@ public abstract class AbstractMailService implements EmailService{
 		return mimeMessage;
 	}
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitacao de nova senha.");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
 }
