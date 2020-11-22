ad37420d431ae2d5346a4fb05ac6e28cc8784c5c
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-21 21:22:51 -0300

    Tratando exceções automaticamente

--- src/main/java/com/paulovarassin/cursomc/services/S3Service.java
+++ src/main/java/com/paulovarassin/cursomc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.paulovarassin.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -47,7 +48,7 @@ public class S3Service {
 			LOG.info("Upload finalizado");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 	
