6a3b03f8049ebc2c3b5d9a1b43a9c66f31a5f266
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-11-21 21:12:38 -0300

    Enviando imagem via endpoint

--- src/main/java/com/paulovarassin/cursomc/services/ClienteService.java
+++ src/main/java/com/paulovarassin/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.paulovarassin.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.paulovarassin.cursomc.domain.Cidade;
 import com.paulovarassin.cursomc.domain.Cliente;
@@ -28,7 +30,10 @@ import com.paulovarassin.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
+	@Autowired
+	private S3Service s3Service;
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
@@ -106,4 +111,9 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
 }
